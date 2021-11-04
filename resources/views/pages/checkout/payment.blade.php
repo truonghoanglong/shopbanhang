d977f03ccaf628664c49a18d81a16b4669b830ab
truonghoanglong <57944514+truonghoanglong@users.noreply.github.com>
2021-11-04 22:33:02 +0700

    '411'

--- resources/views/pages/checkout/payment.blade.php
+++ resources/views/pages/checkout/payment.blade.php
@@ -1,76 +1,90 @@
 
-<link href="{{asset('public/frontend/css/payment.css')}}" rel="stylesheet">
+
 @extends('layout')
 @section('content')
+<link href="{{asset('public/frontend/css/payment.css')}}" rel="stylesheet">
 
-<h2 class='title'>Thanh Toán Giỏ Hàng</h2>
 
-<div class="row">
-  <div class="col-100">
+    
+
+<div class="main">
     <div class="container">
-      <h2>Xem lại gỏi hàng</h2>
-      <div class="table-responsive cart_info">
-        <?php
-        $content = Cart::content();
-        ?>
-        <table class="table table-condensed">
-            <thead>
-                <tr class="cart_menu">
-                    <td class="image">Hình ảnh</td>
-                    <td class="description">Mô tả</td>
-                    <td class="price">Giá</td>
-                    <td class="quantity">Số lượng</td>
-                    <td class="total">Tổng tiền</td>
-                    <td></td>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($content as $v_content)
-                <tr>
-                    <td class="cart_product">
-                        <a href=""><img src="{{URL::to('public/uploads/product/'.$v_content->options->image)}}" width="50" alt=""></a>
-                    </td>
-                    <td class="cart_description">
-                        <h4><a href="">{{$v_content->name}}</a></h4>
-                        <p>Sản phẩm</p>
-                    </td>
-                    <td class="cart_price">
-                        <p>{{number_format($v_content->price,0). ' VND'}}</p>
-                    </td>
-                    <td class="cart_quantity">
-                        <div class="cart_quantity_button">
-                            <form action="{{URL::to('/update-cart-quantity')}}" method="post">
-                            {{csrf_field()}}
-                            
-                            <input class="cart_quantity_input" type="text" name="cart_quantity" value="{{$v_content->qty}}" >
-
-                            <input type="hidden" value="{{$v_content->rowId}}" name="rowId_cart" class="form-control">
-                            <input type="submit" value="Cập nhật" name="update_qty" class="btn btn-default btn-sm">
-
-                            </form>
-                        </div>
-                    </td>
-                    <td class="cart_total">
-                        <p class="cart_total_price">
-                            <?php 
-                                $subtotal = $v_content->price * $v_content->qty;
-                                echo number_format($subtotal).' VND';
-
-                            ?>
-                        </p>
-                    </td>
-                    <td class="cart_delete">
-                        <a class="cart_quantity_delete" href="{{URL::to('delete-to-cart/'.$v_content->rowId)}}"><i class="fa fa-times"></i></a>
-                    </td>
-                </tr>
-                @endforeach
-               
-            </tbody>
-        </table>
-    </div>
-    </div>
-  </div>
- 
+        <h2>Xem lại gỏi hàng</h2>
+        <div class="table-responsive cart_info">    
+          <?php
+          $content = Cart::content();
+          ?>
+          <table class="table table-condensed">
+              <thead>
+                  <tr class="cart_menu">
+                      <td class="image">Hình ảnh</td>
+                      <td class="description">Mô tả</td>
+                      <td class="price">Giá</td>
+                      <td class="quantity">Số lượng</td>
+                      <td class="total">Tổng tiền</td>
+                      <td class="total">Xóa sản phẩm</td>
+                  </tr>
+              </thead>
+              <tbody>
+                  @foreach($content as $v_content)
+                  <tr>
+                      <td class="cart_product">
+                          <a href=""><img src="{{URL::to('public/uploads/product/'.$v_content->options->image)}}" width="50" alt=""></a>
+                      </td>
+                      <td class="cart_description">
+                          <h3><a href="">{{$v_content->name}}</a></h3>
+                          <p>Sản phẩm</p>
+                      </td>
+                      <td class="cart_price">
+                          <p>{{number_format($v_content->price,0). ' VND'}}</p>
+                      </td>
+                      <td class="cart_quantity">
+                          <div class="cart_quantity_button">
+                              <form action="{{URL::to('/update-cart-quantity')}}" method="post">
+                              {{csrf_field()}}
+                              
+                              <input class="cart_quantity_input" type="text" name="cart_quantity" value="{{$v_content->qty}}" >
+  
+                              <input type="hidden" value="{{$v_content->rowId}}" name="rowId_cart" class="form-control">
+                              <input type="submit" value="Cập nhật" name="update_qty" class="btn btn-default btn-sm">
+  
+                              </form>
+                          </div>
+                      </td>
+                      <td class="cart_total">
+                          <p class="cart_total_price">
+                              <?php 
+                                  $subtotal = $v_content->price * $v_content->qty;
+                                  echo number_format($subtotal).' VND';
+  
+                              ?>
+                          </p>
+                      </td>
+                      <td class="cart_delete">
+                          <a class="cart_quantity_delete" href="{{URL::to('delete-to-cart/'.$v_content->rowId)}}"><i class="fa fa-times"></i></a>
+                      </td>
+                  </tr>
+                  @endforeach
+                 
+              </tbody>
+          </table>
+          <h4>Chọn hình thức thanh toán</h4>
+         <form action="{{URL::to('/order-place')}}" method="post">
+            {{csrf_field()}}
+            <div class="payment-option">
+                <span>
+                    <label for=""><input type="checkbox" value='bằng ATM'>Trả bằng ATM</label>
+                </span>
+                <span>
+                    <label for=""><input type="checkbox" value='bằng tiền mặt'>Trả bằng tiền mặt</label>
+                </span>
+                <span>
+                    <label for=""><input type="checkbox" value='thẻ ghi nợ'>Trả qua thẻ ghi nợ</label>
+                </span>
+            </div>
+         </form>
+      
+      </div>
 </div>
 
 
