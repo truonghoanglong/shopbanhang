8ec8d9709d7389bfc069279f120286daeb9432d7
truonghoanglong <57944514+truonghoanglong@users.noreply.github.com>
2022-01-04 23:37:19 +0700

    'fix'

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,4 +1,4 @@
 @extends('admin_layout')
 @section('admin_content')
-
+    {{-- <h1 style="display: flex;justify-content: center;align-items: center; height:80vh">Chào mừng bạn đến với trang quản trị</h1> --}}
 @endsection
